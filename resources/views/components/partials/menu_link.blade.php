708b54816c51aa43fcf695a34e6b0192329559c2
sindibeqo <sindi.beqo@hideolab.al>
2024-05-10 12:16:09 +0200

    working

--- resources/views/components/partials/menu_link.blade.php
+++ resources/views/components/partials/menu_link.blade.php
@@ -1,1 +1,23 @@
-<span> Ka femije</span>
+<div class="group">
+    <p class="link-hover cursor-pointer">{{$link->title}}</p>
+
+    <nav class="absolute w-52 p-4 shadow-lg bg-white hidden group-hover:block">
+        @foreach($link->children as $children)
+            @if ($children->children->count() > 0)
+                @include('components.partials.menu_link', ['link' => $children])
+            @else
+                @php
+                $button_type = $children->type;
+                $url = $children->call_to_action_url;
+                if ($button_type == "internal") {
+                    $url = $children->getRelated('page')->first() != null ? route('frontend.page', [$children->getRelated('page')->first()?->slug]) : '#';
+                }
+                @endphp
+
+                <a href="{{ $url }}" aria-label="{{$children->title}}" @if($children->open_in_new_tab == true) target="_blank" @endif>
+                    {{$children->title}}
+                </a>
+            @endif
+        @endforeach
+    </nav>
+</div>
