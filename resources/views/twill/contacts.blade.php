cd183ff8a5d5d6a71fccb31258d62532b7b08cc8
Oksana <oksana.metushi@hideolab.al>
2024-05-13 17:27:30 +0200

    table fix

--- resources/views/twill/contacts.blade.php
+++ resources/views/twill/contacts.blade.php
@@ -2,40 +2,44 @@
 
 @section('customPageContent')
 
-<table border="1" class="contacts">
-    <thead>
-        <tr>
-            <th>To</th>
-            <th>Subject</th>
-            <th>Success Message</th>
-            <th>Privacy Disclaimer</th>
-            <th>Form data</th>
-            <th>Created At</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($requests as $item)
-        <tr>
-            <td>{{ $item->to }}</td>
-            <td>{{ $item->subject }}</td>
-            <td>{!! $item->success_message !!}</td>
-            <td>{!! $item->privacy_disclaimer !!}</td>
-            <td>
-                <ul>
-                    @foreach($item->form_data as $key => $value)
-                    <li><strong>{{ ucfirst(str_replace('-', ' ', $key)) }}:</strong> {{ $value }}</li>
-                    @endforeach
-                </ul>
-            </td>
-            <td>{{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
+<div class="table-container">
+    <table border="1" class="contacts">
+        <thead>
+            <tr>
+                <th>To</th>
+                <th>Subject</th>
+                <th>Success Message</th>
+                <th>Privacy Disclaimer</th>
+                <th>Form data</th>
+                <th>Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($requests as $item)
+            <tr>
+                <td>{{ $item->to }}</td>
+                <td>{{ $item->subject }}</td>
+                <td>{!! $item->success_message !!}</td>
+                <td>{!! $item->privacy_disclaimer !!}</td>
+                <td>
+                    <ul>
+                        @foreach($item->form_data as $key => $value)
+                        <li><strong>{{ ucfirst(str_replace('-', ' ', $key)) }}:</strong> {{ $value }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>{{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @stop
 
 <style>
+    .table-container {
+        overflow-x: auto;
+    }
     table,
     th,
     td {
