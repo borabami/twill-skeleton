3e8897dc71a9c6a0cb3479657b618662c7ffeaf2
borabami <bora.bami@hideolab.al>
2024-05-15 18:51:06 +0200

    fix contact request

--- app/Http/Controllers/Twill/ContactRequestController.php
+++ app/Http/Controllers/Twill/ContactRequestController.php
@@ -8,6 +8,7 @@ use A17\Twill\Services\Listings\TableColumns;
 use A17\Twill\Services\Forms\Fields\Input;
 use A17\Twill\Services\Forms\Form;
 use A17\Twill\Http\Controllers\Admin\ModuleController as BaseModuleController;
+use A17\Twill\Models\Model;
 
 class ContactRequestController extends BaseModuleController
 {
@@ -26,19 +27,45 @@ class ContactRequestController extends BaseModuleController
         $this->disableBulkEdit();
     }
 
-    
+    protected function getIndexTableColumns(): TableColumns
+    {
+        $columns = new TableColumns();
+
+        $columns->add(
+            Text::make()
+                ->field('to')
+                ->title(twillTrans('To'))
+        );
+
+        $columns->add(
+            Text::make()
+                ->field('form_data')
+                ->title(twillTrans('Form data'))
+                ->customRender(function (Model $model) {
+                    $formDataString = '';
+                    foreach ($model->form_data as $key => $value) {
+                        $formDataString .= '<strong>' . ucwords(str_replace('-', ' ', $key)) . ':</strong> ' . $value . '<br>';
+                    }
+                    return $formDataString;
+                })
+        );
+
+
+        return $columns;
+    }
+
 
     /**
      * This is an example and can be removed if no modifications are needed to the table.
      */
-    protected function additionalIndexTableColumns(): TableColumns
-    {
-        $table = parent::additionalIndexTableColumns();
+    // protected function additionalIndexTableColumns(): TableColumns
+    // {
+    //     $table = parent::additionalIndexTableColumns();
 
-        $table->add(
-            Text::make()->field('description')->title('Description')
-        );
+    //     $table->add(
+    //         Text::make()->field('description')->title('Description')
+    //     );
 
-        return $table;
-    }
+    //     return $table;
+    // }
 }
