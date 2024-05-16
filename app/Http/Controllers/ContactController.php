a83d04af39435abc7cabd691c76311c09937c174
borabami <bora.bami@hideolab.al>
2024-05-16 18:36:56 +0200

    workinng

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\RateLimiter;
 
 class ContactController extends Controller
 {
@@ -18,6 +19,22 @@ class ContactController extends Controller
      */
     public function submitContactForm(Request $request)
     {
+
+        // Define rate limiting parameters
+        $key = 'send-contact-request:' . $request->ip(); // You may want to use a unique identifier for each user
+        $maxAttempts = 3; // Maximum number of attempts allowed
+        $decayMinutes = 1; // Time period for rate limiting (in minutes)
+
+        // Check if the user has exceeded the rate limit
+        if (RateLimiter::tooManyAttempts($key, $maxAttempts, $decayMinutes)) {
+
+            $seconds = RateLimiter::availableIn($key);
+
+            return response()->json(['message' => 'Too many requests. You may try again in ' . $seconds . ' seconds.'], 429);
+        }
+
+        // Increment the attempt counter
+        RateLimiter::increment($key);
 
         // Validate the request data
         $request->validate([
