a835a7ec740910e7a47a251c20cb882919f1b1e1
Oksana <oksana.metushi@hideolab.al>
2024-05-20 16:45:13 +0200

    Frontend fixes

--- resources/views/twill/settings/general/general.blade.php
+++ resources/views/twill/settings/general/general.blade.php
@@ -1,10 +1,16 @@
-@twillBlockTitle('Cache')
+@twillBlockTitle('General')
 @twillBlockIcon('text')
 @twillBlockGroup('app')
 
-<a17-cache-button url="/api/clear-cache"></a17-cache-button>
+<hr />
+
+<x-twill::formFieldset title="Cache" :open="false">
+    <a17-cache-button url="/api/clear-cache"></a17-cache-button>
+</x-twill::formFieldset>
 
 <hr>
 
-<x-twill::medias label="Select the Logo" :max='1' name="logo" />
-<x-twill::medias label="Select Favicon" :max='1' name="favicon" />
+<x-twill::formFieldset title="Logo and Favicon" :open="false">
+    <x-twill::medias label="Select the Logo" :max='1' name="logo" />
+    <x-twill::medias label="Select Favicon" :max='1' name="favicon" />
+</x-twill::formFieldset>
